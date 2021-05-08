4dba653296c32185913ebded2382b039b07c8f0c
xhartono <xhartono@gmail.com>
2021-05-08 22:23:34 +0700

    updating mysql and nodejs source code

--- nodejs-microservice/index.js
+++ nodejs-microservice/index.js
@@ -17,7 +17,7 @@ const HOST = process.env.HOST || '0.0.0.0';
 
 // mysql credentials
 const connection = mysql.createConnection({
-	host: process.env.MYSQL_HOST || '172.17.0.2',
+	host: process.env.MYSQL_HOST || 'localhost:1111',
 	user: process.env.MYSQL_USER || 'root',
 	password: process.env.MYSQL_PASSWORD || 'password',
 	database: process.env.MYSQL_DATABASE || 'test'
